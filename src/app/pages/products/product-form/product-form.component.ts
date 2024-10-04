cf5f27997efddb4a8d1afa622abcc2ad6d6b5587
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-10-04 17:44:26 -0500

    [+] (register,product-form).component: HTML & CSS

--- src/app/pages/products/product-form/product-form.component.ts
+++ src/app/pages/products/product-form/product-form.component.ts
@@ -1,10 +1,11 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product-form',
   standalone: true,
-  imports: [ ReactiveFormsModule ],
+  imports: [ ReactiveFormsModule, NgIf ],
   templateUrl: './product-form.component.html',
   styleUrl: './product-form.component.css'
 })
@@ -14,20 +15,25 @@ export class ProductFormComponent {
   constructor () {
     /** Define la agrupación de campos del formulario */
     this.productForm = new FormGroup({
-      name: new FormControl( '' ),
+      name: new FormControl( '', [ Validators.required ] ),
       description: new FormControl( '' ),
-      price: new FormControl( 0 ),
-      quantity: new FormControl( 0 ),
-      category: new FormControl( '' ),
+      price: new FormControl( 0, [ Validators.required, Validators.min( 0 ) ] ),
+      quantity: new FormControl( 1, [ Validators.required, Validators.min( 1 ) ] ),
+      category: new FormControl( '', [ Validators.required ] ),
       urlImage: new FormControl( '' )
     });
   }
 
   /** Metodo que obtiene los valores de los campos del formulario */
   onSubmit() {
-    const formData = this.productForm.value;    // Datos del formulario
-    console.log( formData );  
-    this.productForm.reset();                   // Limpia los campos del formulario
+    // Verifica si el formulario es valido de acuerdo las validaciones del formulario
+    if( this.productForm.valid ) {
+      const formData = this.productForm.value;    // Datos del formulario
+      console.log( formData );  
+      this.productForm.reset();                   // Limpia los campos del formulario
+    }
+
+    
   }
 
 }
