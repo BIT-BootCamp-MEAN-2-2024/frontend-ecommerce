4e0527066f82acd1520057c21c76ca7c1b012238
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-10-04 18:46:10 -0500

    [+] register.component*: Define formulario reactivo

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  formData!: FormGroup;
+
+  constructor() {
+    this.formData = new FormGroup({
+      name: new FormControl( '', [ Validators.required ] ),
+      username: new FormControl( '', [ Validators.required, Validators.email ] ),
+      password: new FormControl( '', [ Validators.required, Validators.minLength( 8 ), Validators.maxLength( 20 ) ] )
+    });
+  }
+
+  handleSubmit() {
+    if(this.formData.valid) {
+      console.log(this.formData.value);
+      this.formData.reset();
+    }
+  }
 
 }
