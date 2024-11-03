f80d23fe8fcc4d54682ef1055b45cc30d27f801d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-11-03 15:09:04 -0500

    [+] checkout.component: Actualiza componente renderizandolo (se recomienda implementar BehaviorSubject de RxJS)

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -26,19 +26,39 @@ export class CheckoutComponent {
   addOneItem( id: any ) {
     console.log( 'Agrega 1 item con ID: ' + id );
     this.cartService.addOneItem( id );
+
+    // Esto recargará el componente de pagina, se recomienda usar BehaviorSubject 
+    // que permite lograr la reactividad deseada en el componente que lo consume de 
+    // manera que este actualiza sus datos sin tener que renderezarse completamente
+    this.cartProducts = this.cartService.items;    
   }
 
   removeOneItem( id: any ) {
     console.log( 'Elimina 1 item con ID: ' + id );
     this.cartService.removeOneItem( id );
+
+    // Esto recargará el componente de pagina, se recomienda usar BehaviorSubject 
+    // que permite lograr la reactividad deseada en el componente que lo consume de 
+    // manera que este actualiza sus datos sin tener que renderezarse completamente
+    this.cartProducts = this.cartService.items;
   }
 
   removeToCart( id: any ) {
     console.log( 'Elimina todos los items con ID: ' + id );
     this.cartService.removeToCart( id );
+    
+    // Esto recargará el componente de pagina, se recomienda usar BehaviorSubject 
+    // que permite lograr la reactividad deseada en el componente que lo consume de 
+    // manera que este actualiza sus datos sin tener que renderezarse completamente
+    this.cartProducts = this.cartService.items;
   }
 
   removeAllItems() {
     console.log( 'Elimina todos los items' );
+    
+    // Esto recargará el componente de pagina, se recomienda usar BehaviorSubject 
+    // que permite lograr la reactividad deseada en el componente que lo consume de 
+    // manera que este actualiza sus datos sin tener que renderezarse completamente
+    this.cartProducts = this.cartService.items;
   }
 }
