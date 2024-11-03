746a2736b828e72b4a61913f10701f88dcfaae04
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-11-02 22:45:38 -0500

    [+] checkout.component: Estructura y despliege de los productos en el carrito de compras

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css'
 })
 export class CheckoutComponent {
+  cartProducts: any[] = [];
 
+  constructor( private cartService: CartService ) {}
+
+  ngOnInit() {
+    this.cartProducts = this.cartService.items;
+    console.log( this.cartProducts );
+  }
 }
